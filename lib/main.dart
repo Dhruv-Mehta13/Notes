f0adcae28b6bf1bba331511a509ac40b0281c823
Dhruv-Mehta13 <mehtadhruv383@gmail.com>
2024-02-04 11:55:39 +0530

    edit and delete

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfire/screens/SplashScreen.dart';
+import 'package:flutterfire/screens/auth.dart';
 import 'package:flutterfire/screens/home.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -19,6 +20,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      routes: {
+        '/auth': (context) => AuthPage(),
+      },
       debugShowCheckedModeBanner: false,
       home: FirebaseAuth.instance.currentUser != null ? Home() : SplashScreen(),
     );
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutterfire/screens/auth.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -27,7 +28,7 @@ class Home extends StatelessWidget {
           IconButton(
             onPressed: () async {
               await FirebaseAuth.instance.signOut();
-              exit(0);
+              Navigator.popUntil(context, (ModalRoute.withName('/')));
             },
             icon: Icon(
               Icons.logout,
@@ -64,7 +65,6 @@ class _NoteAreaState extends State<NoteArea> {
       if (currentUser != null) {
         String userCollection = 'users/${currentUser.uid}/notes';
 
-        // Use the provided title for each note
         await FirebaseFirestore.instance.collection(userCollection).add({
           'title': _titleController.text,
           'note': _noteController.text,
@@ -86,17 +86,27 @@ class _NoteAreaState extends State<NoteArea> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueGrey[50],
       appBar: AppBar(
-        title: TextField(
-          controller: _titleController,
-          style: TextStyle(
-            fontFamily: 'Poppins',
-            color: Colors.black,
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.blueGrey[50],
+        title: Container(
+          padding: EdgeInsets.symmetric(horizontal: 8.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.blueGrey[100],
           ),
-          decoration: InputDecoration(
-            border: InputBorder.none,
-            hintText: 'Enter Title',
-            hintStyle: TextStyle(fontWeight: FontWeight.w700),
+          child: TextField(
+            controller: _titleController,
+            style: TextStyle(
+              fontFamily: 'Poppins',
+              color: Colors.black,
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: 'Enter Title',
+              hintStyle: TextStyle(fontWeight: FontWeight.w700),
+            ),
           ),
         ),
         bottom: PreferredSize(
@@ -109,7 +119,7 @@ class _NoteAreaState extends State<NoteArea> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Card(
-          color: Colors.white,
+          color: Colors.blueGrey[50],
           elevation: 5.0,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10.0),
@@ -178,14 +188,28 @@ class NotesList extends StatelessWidget {
           }
 
           return ListView.builder(
-            itemCount: notes.length,
-            itemBuilder: (context, index) {
-              return NoteContainer(
-                title: notes[index]['title'],
-                note: notes[index]['note'],
-              );
-            },
-          );
+              itemCount: notes.length,
+              itemBuilder: (context, index) {
+                return Dismissible(
+                  key: Key(notes[index].id),
+                  child: NoteContainer(
+                    userCollection: userCollection,
+                    id: notes[index].id,
+                    title: notes[index]['title'],
+                    note: notes[index]['note'],
+                  ),
+                  onDismissed: (direction) {
+                    notes[index].reference.delete();
+                  },
+                  background: Container(
+                    color: Colors.red,
+                    child: Icon(Icons.delete,
+                        color: Color.fromARGB(255, 254, 254, 254), size: 30.0),
+                    alignment: Alignment.centerRight,
+                    padding: EdgeInsets.only(right: 16.0),
+                  ),
+                );
+              });
         }
       },
     );
@@ -193,40 +217,172 @@ class NotesList extends StatelessWidget {
 }
 
 class NoteContainer extends StatelessWidget {
+  final String id;
   final String title;
   final String note;
-
-  NoteContainer({required this.title, required this.note});
+  final String userCollection;
+  NoteContainer(
+      {super.key,
+      required this.title,
+      required this.note,
+      required this.id,
+      required this.userCollection});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(8.0),
-      padding: EdgeInsets.all(16.0),
-      decoration: BoxDecoration(
-        color: Colors.blueGrey[100],
-        borderRadius: BorderRadius.circular(8.0),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => EditnoteScreen(
+                    id: id,
+                    initialNote: title,
+                    initialTitle: note,
+                    userCollection: userCollection)));
+      },
+      child: Container(
+        width: double.infinity,
+        margin: EdgeInsets.all(8.0),
+        padding: EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          color: Colors.blueGrey[100],
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Poppins',
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              note,
+              style: TextStyle(
+                fontSize: 16.0,
+                fontFamily: 'Poppins',
+              ),
+            ),
+          ],
+        ),
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            title,
-            style: TextStyle(
-              fontSize: 18.0,
-              fontWeight: FontWeight.bold,
-              fontFamily: 'Poppins',
-            ),
+    );
+  }
+}
+
+class EditnoteScreen extends StatefulWidget {
+  const EditnoteScreen(
+      {super.key,
+      required this.id,
+      required this.initialTitle,
+      required this.initialNote,
+      required this.userCollection});
+  final String id;
+  final String initialTitle;
+  final String initialNote;
+  final String userCollection;
+  @override
+  State<EditnoteScreen> createState() => _EditnoteScreenState();
+}
+
+class _EditnoteScreenState extends State<EditnoteScreen> {
+  final TextEditingController _editedTitleController = TextEditingController();
+  final TextEditingController _editedNoteController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    _editedTitleController.text = widget.initialTitle;
+    _editedNoteController.text = widget.initialNote;
+  }
+
+  void saveEditedNote() async {
+    try {
+      String editedTitle = _editedTitleController.text;
+      String editedNote = _editedNoteController.text;
+
+      await FirebaseFirestore.instance
+          .collection(widget.userCollection)
+          .doc(widget.id)
+          .update({
+        'title': editedTitle,
+        'note': editedNote,
+      });
+
+      Navigator.pop(context);
+    } catch (e) {
+      print('Error updating note: $e');
+    }
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          controller: _editedTitleController,
+          style: TextStyle(
+            fontFamily: 'Poppins',
+            color: Colors.black,
           ),
-          SizedBox(height: 8.0),
-          Text(
-            note,
-            style: TextStyle(
-              fontSize: 16.0,
-              fontFamily: 'Poppins',
-            ),
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            hintText: 'Enter Title',
+            hintStyle: TextStyle(fontWeight: FontWeight.w700),
           ),
-        ],
+        ),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(1),
+          child: Divider(
+            thickness: 3,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Card(
+          color: Colors.white,
+          elevation: 5.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _editedNoteController,
+                  cursorColor: Colors.black,
+                  maxLines: null,
+                  decoration: InputDecoration(
+                    hintStyle: TextStyle(
+                      fontFamily: 'Poppins',
+                      height: 1.5,
+                    ),
+                    border: InputBorder.none,
+                    hintText: 'Write your note Here  ................',
+                  ),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Implement saving edited note to Firestore
+                  saveEditedNote();
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 20.0),
+                  backgroundColor: Colors.blueGrey[100],
+                ),
+                child: Text('Save Changes',
+                    style: TextStyle(fontSize: 16.0, color: Colors.black)),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
